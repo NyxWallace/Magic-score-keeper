c145e20e8f308947f2e132af02f38eda481be8db
Antoine Farine <antoine.farine@hotmail.com>
2018-07-25 21:02:58 +0200

    Added solution 8.1 (1st part)

---
+++ Windows 8.1/Magic score keeper/Magic the gathering/Model/Player.cs
@@ -0,0 +1,63 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic_the_gathering.Model
+{
+    public class Player : ViewModelBase
+    {
+        private string _name;
+        private int _hp;
+        private int _poisonCounter;
+
+        public string Name
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_name))
+                    _name = "Player";
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        public int HP
+        {
+            get
+            {
+                return _hp;
+            }
+            set
+            {
+                _hp = value;
+                RaisePropertyChanged("HP");
+            }
+        }
+
+        public int PoisonCounter
+        {
+            get
+            {
+                return _poisonCounter;
+            }
+            set
+            {
+                _poisonCounter = value;
+                RaisePropertyChanged("PoisonCounter");
+            }
+        }
+
+        public Player(string name)
+        {
+            this._name = name;
+            RaisePropertyChanged(() => Name);
+        }
+    }
+}
