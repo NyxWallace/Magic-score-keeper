1852a45cedb6c3107f1250cc5e716c6158738422
AntoineFarine <antoine.farine@student.supsi.ch>
2018-07-26 23:38:51 +0200

    Added UWP

---
+++ Universal Windows/Infrastructure/MagicScoreKeeper.Infrastructure.ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicScoreKeeper.Infrastructure.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private PlayerViewModel _player1 = null;
+        private PlayerViewModel _player2 = null;
+
+        public PlayerViewModel Player1
+        {
+            get
+            {
+                if(_player1 == null)
+                {
+                    _player1 = new PlayerViewModel("Player 1");
+                }
+                return _player1;
+            }
+            set
+            {
+                if (_player1 == value) return;
+                _player1 = value;
+                RaisePropertyChanged(() => Player1);
+            }
+        }
+
+        public PlayerViewModel Player2
+        {
+            get
+            {
+                if (_player2 == null)
+                {
+                    _player2 = new PlayerViewModel("Player 2");
+                }
+                return _player2;
+            }
+            set
+            {
+                if (_player2 == value) return;
+                _player2 = value;
+                RaisePropertyChanged(() => Player2);
+            }
+        }
+    }
+}
---
+++ Universal Windows/Infrastructure/MagicScoreKeeper.Infrastructure.ViewModel/PlayerViewModel.cs
@@ -0,0 +1,91 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MagicScoreKeeper.Infrastructure.ViewModel
+{
+    public class PlayerViewModel : ViewModelBase
+    {
+        private int _hp = 20;
+        private int _poison = 0;
+        private string _name = string.Empty;
+        private RelayCommand<int> _increaseHPCommand = null;
+        private RelayCommand<int> _increasePoisonCommand = null;
+
+        public int HP
+        {
+            get
+            {
+                return _hp;
+            }
+            set
+            {
+                if (_hp == value) return;
+                _hp = value;
+                RaisePropertyChanged(() => HP);
+            }
+        }
+
+        public int Poison
+        {
+            get
+            {
+                return _poison;
+            }
+            set
+            {
+                if (_poison == value) return;
+                _poison = value;
+                RaisePropertyChanged(() => Poison);
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name == value) return;
+                _name = value;
+                RaisePropertyChanged(() => Name);
+            }
+        }
+
+        public ICommand IncreaseHPCommand
+        {
+            get
+            {
+                return _increaseHPCommand ?? (_increaseHPCommand = new RelayCommand<int>((val) =>
+                {
+                    HP += val;
+                }));
+            }
+        }
+
+        public ICommand IncreasePoisonCommand
+        {
+            get
+            {
+                return _increasePoisonCommand ?? (_increasePoisonCommand = new RelayCommand<int>((val) =>
+                {
+                    Poison += val;
+                }));
+            }
+        }
+
+        public PlayerViewModel() { }
+
+        public PlayerViewModel(string name) : this()
+        {
+            _name = name;
+        }
+    }
+}
