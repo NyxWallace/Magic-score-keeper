39ff3399f42b674b0bfabcb3cb1d7af614fe389b
AntoineFarine <antoine.farine@student.supsi.ch>
2018-07-26 23:46:16 +0200

    Moved some folders

---
+++ Windows 8.1/Magic the gathering/Counter.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ViewManagement;
+
+namespace Magic_the_gathering
+{
+    public sealed partial class Counter : Magic_the_gathering.Common.LayoutAwarePage
+    {
+        public Counter()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+        }
+
+        //private void pageRoot_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    if (ApplicationView.Value == ApplicationViewState.Snapped)
+        //    {
+        //        NormalGrid.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        //        SnappedGrid.Visibility = Windows.UI.Xaml.Visibility.Visible;
+        //    }
+        //    else
+        //    {
+        //        NormalGrid.Visibility = Windows.UI.Xaml.Visibility.Visible;
+        //        SnappedGrid.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        //    }
+        //}
+    }
+}
